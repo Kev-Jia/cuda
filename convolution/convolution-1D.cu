bb8c8b4465179e33303d856bf988d1eefa4b5c5a
Kevin Jia <kevinhengkaijia@gmail.com>
2020-12-23 18:19:53 +0000

    added directories, altered some comments and added convolution files

---
+++ convolution/convolution-1D.cu
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// CUDA kernel function
+__global__ void convolution_1D_Kernel(float* d_m, float* d_mask, float* d_n, size_t length, size_t maskLength)
+{
+    // define and initialize the variable that will be used for indexing
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    // define and initialize the variable that will allow the mask to align correctly with input array d_m
+    // integer division is fine because it always truncates
+    // we will only be using odd values for maskLength anyway
+    // the truncation of integer division will provide the perfect offset for aligning the centre element of the mask with the target d_m element
+    int m_startingIndex = i - (maskLength / 2);
+
+    // initialize the to-be-generated result element of result array d_n to be 0 ready for accumulation in the for loop directly below
+    d_n[i] = 0;
+
+    // iterate through all the elements in the mask
+    // here's where the actual convolution operation will occur
+    for(int j = 0; j < maskLength; j++)
+    {
+        // this if statement is a boundary condition check
+        // it checks whether the indexes needed for the convolution operation are within the bounds of the input array
+        // if they are not
+        // extra "ghost" elements past the beginning and end of the input array are used
+        // these elements are set to 0 in our code
+        // this ghost element stuff is done implicitly
+        // as there is no need to do it explicitly, since 0 does not change the resulting element of the convolution operation on a specific element
+        // we just leave the accumulating result of the convolution operation alone if the indexes are out of bounds
+        if(m_startingIndex + j >= 0 && m_startingIndex + j < length)
+        {
+            // if the boundary check is satisfied
+            // then accumulate one part of the convolution operation to the result element of the result d_n array
+            d_n[i] += d_m[m_startingIndex + j] * d_mask[j];
+        }
+    }
+}
+
+// error checking function - checks for CUDA errors
+void errorCheck(unsigned int line)
+{
+    // get most recent CUDA error
+    cudaError_t cudaError = cudaGetLastError();
+
+    // if error code wasn't a code describing success
+    if(cudaError != cudaSuccess)
+    {
+        // output that there has been a CUDA error in the line of the CUDA function call
+        // and exit the program
+        printf("CUDA error in line %u in file %s: %s\n", line - 1, __FILE__, cudaGetErrorString(cudaError));
+        exit(EXIT_FAILURE);
+    }
+}
+
+// host function that calls the CUDA kernel
+void convolution_1D(float* m, float* mask, float* n, size_t length, size_t maskLength)
+{
+    // define and initialize dimension variables containing data regarding the dimensions of the grid and the dimensions of each block
+    dim3 numOfBlocks(ceil(length / 64.0), 1, 1);
+    dim3 numOfThreads(64, 1, 1);
+
+    // define and initialize variables containing the number of bytes in each array
+    size_t bytes_m = length * sizeof(float);
+    size_t bytes_mask = maskLength * sizeof(float);
+    size_t bytes_n = length * sizeof(float);
+
+    // define the pointers that will point to the start of allocated device memory for each array
+    float* d_m;
+    float* d_mask;
+    float* d_n;
+
+    // allocate global memory for each array on the device and check for CUDA errors
+    cudaMalloc((void**) &d_m, bytes_m);
+    errorCheck(__LINE__);
+    cudaMalloc((void**) &d_mask, bytes_mask);
+    errorCheck(__LINE__);
+    cudaMalloc((void**) &d_n, bytes_n);
+    errorCheck(__LINE__);
+
+    // copy the data of each array to allocated global memory on the device and check for CUDA errors
+    cudaMemcpy(d_m, m, bytes_m, cudaMemcpyHostToDevice);
+    errorCheck(__LINE__);
+    cudaMemcpy(d_mask, mask, bytes_mask, cudaMemcpyHostToDevice);
+    errorCheck(__LINE__);
+
+    // call the CUDA kernel and check for CUDA errors
+    convolution_1D_Kernel<<<numOfBlocks, numOfThreads>>>(d_m, d_mask, d_n, length, maskLength);
+    errorCheck(__LINE__);
+    
+    // copy the data of the result array from global memory to host DRAM and check for CUDA errors
+    cudaMemcpy(n, d_n, bytes_n, cudaMemcpyDeviceToHost);
+    errorCheck(__LINE__);
+    
+    // free the allocated global memory and check for CUDA errors
+    cudaFree(d_m);
+    errorCheck(__LINE__);
+    cudaFree(d_mask);
+    errorCheck(__LINE__);
+    cudaFree(d_n);
+    errorCheck(__LINE__);
+}
+
+int main()
+{
+    // define structs that will enable us to get the exec time of the program
+    struct timespec start, end;
+
+    // get the details regarding the start time of this program and store it in the start struct
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    // initialize pseudo-random number generator with seed of current seconds since 01/01/1970
+    srand(time(NULL));
+    
+    // define and initialize size variables for each array
+    // the input and result arrays have the same size and thus share a size variable
+    size_t length = rand() % 65537 + 983040;
+    size_t maskLength = 2 * (rand() % 2049 + 30720) - 1;
+
+    // dynamically allocate DRAM memory for the arrays to account for them perhaps being too big to be statically allocated
+    float* m = (float*) malloc(length * sizeof(float));
+    float* mask = (float*) malloc(maskLength * sizeof(float));
+    float* n = (float*) malloc(length * sizeof(float));
+
+    // assign a pseudo-random integer value from -64 to 64 for each element in input array m
+    for(int i = 0; i < length; ++i)
+    {
+        m[i] = rand() % 129 - 64;
+    }
+  
+    // assign a pseudo-random float value from 0 to 1 with a precision of 3 decimal places for each element in mask array
+    for(int j = 0; j < maskLength; ++j)
+    {
+       mask[j] =  rand() % 1001 / 1000.0;
+    }
+
+    // perform 1D convolution operation on input array m using a given mask array
+    convolution_1D(m, mask, n, length, maskLength);
+
+    // get the details regarding the end time of this program and store it in the end struct
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    // calculate exec time in microseconds
+    time_t execTime = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+
+    // output exec time
+    printf("Execution time: %d microseconds.", execTime);
+
+    // exit program
+    return 0;
+}
---
+++ matrixMultiplication/matrixMultiplication2.cu
@@ -0,0 +1,114 @@
+// include necessary libs
+#include <cuda.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// CUDA kernel function
+__global__ void multiplyMatricesKernel(float* d_x, float* d_y, float* d_z, int m, int n, int p)
+{
+    // define and initialize the variable that will be used in indexing - this is for brevity
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    // only have threads within the range of the amount of rows of the result matrix to calculate an element of it
+    // this is to prevent global memory segfaults
+    if(i < m)
+    {
+        // calculate each row of the result matrix
+        for(int j = 0; j < p; ++j)
+        {
+            for(int k = 0; k < n; ++k)
+            {
+                d_z[i * p + j] += d_x[i * n + k] * d_y[k * p + j];
+            }
+        }
+    }
+}
+
+// host function that calls CUDA kernel
+void multiplyMatrices(float* x, float* y, float* z, int m, int n, int p)
+{
+    // define and initialize dimension variables containing data regarding the dimensions of the grid and dimensions of each block
+    dim3 numOfBlocks(ceil(m / 64.0), 1, 1);
+    dim3 numOfThreads(64, 1, 1);
+
+    // define and initialize the variables containing number of bytes in each matrix 
+    int bytes_x = m * n * sizeof(float);
+    int bytes_y = n * p * sizeof(float);
+    int bytes_z = m * p * sizeof(float);
+
+    // define the pointers that will point to the start of allocated device memory for each matrix
+    float* d_x;
+    float* d_y;
+    float* d_z;
+
+    // allocate global memory for the matrices on the device
+    cudaMalloc((void**) &d_x, bytes_x);
+    cudaMalloc((void**) &d_y, bytes_y);
+    cudaMalloc((void**) &d_z, bytes_z);
+
+    // copy the data of input matrices to the allocated global memory on the device
+    cudaMemcpy(d_x, x, bytes_x, cudaMemcpyHostToDevice); 
+    cudaMemcpy(d_y, y, bytes_y, cudaMemcpyHostToDevice);
+
+    // call the CUDA kernel
+    multiplyMatricesKernel<<<numOfBlocks, numOfThreads>>>(d_x, d_y, d_z, m, n, p);
+    
+    // copy the data of the result matrix from global memory to host DRAM
+    cudaMemcpy(z, d_z, bytes_z, cudaMemcpyDeviceToHost);
+    
+    // free the allocated global memory
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+}
+
+int main()
+{
+    // define structs that will enable us to get the exec time of the program
+    struct timespec start, end;
+
+    // get the details regarding the start time of this program and store it in the start struct
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    // initialize pseudo-random number generator with seed of the current seconds since 01/01/1970
+    srand(time(NULL));
+    
+    // define and initialize dimension variables for the 3 matrices
+    // these variables will have values in a range from 3840 to 4096
+    size_t m = rand() % 257 + 3840;
+    size_t n = rand() % 257 + 3840;
+    size_t p = rand() % 257 + 3840;
+
+    // dynamically allocate DRAM memory for the matrices to account for them being pehaps too big to be statically allocated
+    float* x = (float*) malloc(m * n * sizeof(float));
+    float* y = (float*) malloc(n * p * sizeof(float));
+    float* z = (float*) malloc(m * p * sizeof(float));
+
+    // assign a pseudo-random integer value from -64 to 64 for each element in input matrix x
+    for(int i = 0; i < sizeof(x) / sizeof(float); ++i)
+    {
+        x[i] = rand() % 129 - 64;
+    }
+        
+    // assign a pseudo-random integer value from -64 to 64 for each element in input matrix y
+    for(int i = 0; i < sizeof(y) / sizeof(float); ++i)
+    {
+        y[i] = rand() % 129 - 64;
+    }
+    
+    // multiply the input matrices x and y
+    multiplyMatrices(x, y, z, m, n, p);
+    
+    // get the details regarding the end time of this program and store it in the end struct
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    // calculate exec time in microseconds
+    time_t execTime = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+
+    // output exec time
+    printf("Execution time: %d microseconds.", execTime);
+
+    // exit program
+    return 0;
+}
