727b0c7b0fa8850770ac3e05ead8ff1df156cbec
Kevin Jia <kevinhengkaijia@gmail.com>
2020-12-23 16:09:54 +0000

    changed host memory allocation from static to dynamic
    done to account for segfaults regarding too big matrices

--- matrixMultiplication2.cu
+++ matrixMultiplication2.cu
@@ -80,10 +80,10 @@ int main()
     size_t n = rand() % 257 + 3840;
     size_t p = rand() % 257 + 3840;
 
-    // statically define all the matrices
-    float x[m * n];
-    float y[n * p];
-    float z[m * p];
+    // dynamically allocate DRAM memory for the matrices to account for the matrices being pehaps too big to be statically allocated
+    float x = (float*) malloc(m * n * sizeof(float));
+    float y = (float*) malloc(n * p * sizeof(float));
+    float z = (float*) malloc(m * p * sizeof(float));
 
     // assign a pseudo-random value from -64 to 64 for each element in input matrix x
     for(int i = 0; i < sizeof(x) / sizeof(float); ++i)
